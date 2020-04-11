bf9b0393344989f99f9ed225ce53e98f18e1c3c9
Graham Farrell <grahamfarrell1984@gmail.com>
2020-04-11 13:46:30 +0100

    Testing code.

--- mqtt.py
+++ mqtt.py
@@ -126,7 +126,7 @@ def publish(publish):
             'motion': motion
         }
         client.publish("GFNCI/PUBLISH", json.dumps(readings))
-        time.sleep(5)
+        time.sleep(1)
 
 def start_client(client_id):
     client = mqtt.Client(client_id)
