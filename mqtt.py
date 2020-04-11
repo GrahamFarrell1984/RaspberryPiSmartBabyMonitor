5607e97be1bb1e5a24ad3ee7c97ad72abffa04dd
Graham Farrell <grahamfarrell1984@gmail.com>
2020-04-11 03:15:56 +0100

    Testing code.

--- mqtt.py
+++ mqtt.py
@@ -19,8 +19,8 @@ publishing = False
 # dht_sensor = 7 # Connect the DHT sensor to digital port D7
 # dht_sensor_type = 0 # Use 0 for the blue-colored sensor
 #
-# light_sensor = 0 # Connect the Grove Light Sensor to analog port A0
-# grovepi.pinMode(light_sensor,"INPUT") # Set pin mode for port A0 as an input
+light_sensor = 0 # Connect the Grove Light Sensor to analog port A0
+grovepi.pinMode(light_sensor,"INPUT") # Set pin mode for port A0 as an input
 #
 # sound_sensor = 1 # Connect the Grove Sound Sensor to analog port A1
 # grovepi.pinMode(sound_sensor,"INPUT") # Set pin mode for port A1 as an input
@@ -45,22 +45,22 @@ publishing = False
 #     return 20
 
 # Method to read from light sensor
-def read_light():
-    # light_sensor_value = grovepi.analogRead(light_sensor) # Read the light sensor value and store it in a variable called light_sensor_value
-    # return light_sensor_value # Return the value from the light sensor
-    return 30
-
-# Method to read from sound sensor
-def read_sound():
-    # sound_sensor_value = grovepi.analogRead(sound_sensor) # Read the sound sensor value and store it in a variable called sound_sensor_value
-    # return sound_sensor_value # Return the value from the sound sensor
-    return 40
-
-# Method to read from pir sensor
-def read_pir():
-    # pir_sensor_value = grovepi.digitalRead(pir_sensor) # Read the pir sensor value and store it in a variable called pir_sensor_value
-    # return pir_sensor_value # Return the value from the pir sensor
-    return 1
+# def read_light():
+#     # light_sensor_value = grovepi.analogRead(light_sensor) # Read the light sensor value and store it in a variable called light_sensor_value
+#     # return light_sensor_value # Return the value from the light sensor
+#     return 30
+#
+# # Method to read from sound sensor
+# def read_sound():
+#     # sound_sensor_value = grovepi.analogRead(sound_sensor) # Read the sound sensor value and store it in a variable called sound_sensor_value
+#     # return sound_sensor_value # Return the value from the sound sensor
+#     return 40
+#
+# # Method to read from pir sensor
+# def read_pir():
+#     # pir_sensor_value = grovepi.digitalRead(pir_sensor) # Read the pir sensor value and store it in a variable called pir_sensor_value
+#     # return pir_sensor_value # Return the value from the pir sensor
+#     return 1
 
 # Method to read from temperature sensor
 def read_temperature():
@@ -73,12 +73,12 @@ def read_humidity():
     [ temp_sensor_value, hum_sensor_value ] = dht(dht_sensor, dht_sensor_type) # Read the temperature and humidity sensor values
     humidity = str(hum_sensor_value) # Convert humidity sensor value to a String and store in a variable called humidity
     return humidity # Return the humidity value from the dht sensor
-#
-# # Method to read from light sensor
-# def read_light():
-#     light_sensor_value = grovepi.analogRead(light_sensor) # Read the light sensor value and store it in a variable called light_sensor_value
-#     return light_sensor_value # Return the value from the light sensor
-#
+
+# Method to read from light sensor
+def read_light():
+    light_sensor_value = grovepi.analogRead(light_sensor) # Read the light sensor value and store it in a variable called light_sensor_value
+    return light_sensor_value # Return the value from the light sensor
+
 # # Method to read from sound sensor
 # def read_sound():
 #     sound_sensor_value = grovepi.analogRead(sound_sensor) # Read the sound sensor value and store it in a variable called sound_sensor_value
@@ -111,20 +111,20 @@ def publish(publish):
             # temperature = read_temperature() # Call the read_temperature() function / method and store result in a variable called temperature
             # humidity = read_humidity() # Call the read_humidity() function / method and store result in a variable called humidity
             light = read_light() # Call the read_light() function / method and store result in a variable called light
-            sound = read_sound() # Call the read_sound() function / method and store result in a variable called sound
-            motion = read_pir() # Call the read_pir() function / method and store result in a variable called motion
+            # sound = read_sound() # Call the read_sound() function / method and store result in a variable called sound
+            # motion = read_pir() # Call the read_pir() function / method and store result in a variable called motion
             print("While loop has run " + str(counter) + " times" if counter > 1 else "While loop has run " + str(counter) + " time")
             # print("Temperature: " + str(temperature))
             # print("Humidity: " + str(humidity))
             print("Light: " + str(light))
-            print("Sound: " + str(sound))
-            print("Motion detected" if motion else "No motion detected")
+            # print("Sound: " + str(sound))
+            # print("Motion detected" if motion else "No motion detected")
             readings = {
                 # 'temperature': temperature,
                 # 'humidity': humidity,
-                'light': light,
-                'sound': sound,
-                'motion': motion
+                'light': light
+                # 'sound': sound,
+                # 'motion': motion
             }
             client.publish("GFNCI/PUBLISH", json.dumps(readings))
             time.sleep(2)
